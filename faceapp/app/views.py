82d495a84ab4579a2e3b8bcaaa14fec27c34626f
zinrack <71554865+zinrack@users.noreply.github.com>
2020-12-03 17:51:42 +0000

    Add files via upload

---
+++ faceapp/app/views.py
@@ -0,0 +1,42 @@
+from flask import render_template, request
+from flask import redirect, url_for
+import os
+from PIL import Image
+from app.utils import pipeline_model
+
+UPLOAD_FLODER = 'static/uploads'
+def base():
+    return render_template('base.html')
+
+
+def index():
+    return render_template('index.html')
+
+
+
+
+
+##  this function for uploading the photo
+def getwidth(path):
+    img = Image.open(path)
+    size = img.size 
+    aspect = size[0]/size[1] 
+    w = 300 * aspect
+    return int(w)
+
+def gender():
+
+    if request.method == "POST":
+        f = request.files['image']
+        filename=  f.filename
+        path = os.path.join(UPLOAD_FLODER,filename)
+        f.save(path)
+        w = getwidth(path)
+        # the pipeline
+        pipeline_model(path,filename,color='bgr')
+
+
+        return render_template('gender.html',fileupload=True,img_name=filename, w=w)
+
+
+    return render_template('gender.html',fileupload=False,img_name="freeai.png")
